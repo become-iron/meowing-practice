0c79870905a5930d850c1640869685427e5ea237
Nikita Burkov <nikitaburkov@yandex.ru>
2020-07-11 18:09:46 +0300

    Fix

--- sorting/bubble/become-iron/bubble.ts
+++ sorting/bubble/become-iron/bubble.ts
@@ -6,8 +6,7 @@ export function bubbleSort(array: Array<string | number>): Array<string | number
     for (let i = 0; i < array.length - 1; i++) {
       if (array[i] > array[i + 1]) {
         sorted = false
-        array[i] = array[i + 1]
-        array[i + 1] = array[i]
+        ;[array[i], array[i + 1]] = [array[i + 1], array[i]]
       }
     }
 
