9c5edfb6101b28efe56deab919b4d445302ca6fa
Paragen <k-paradox-p@mail.ru>
2020-07-13 15:52:44 +0300

    Add merge and selection sort. Add initial tests.

---
+++ sorting/merge/merge.h
@@ -0,0 +1,35 @@
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include "helpers.h"
+
+template <class Iter, class Comp = DefaultComparator<typename Iter::value_type>>
+void mergeSort(Iter begin, Iter end, const Comp& comp = Comp()) {
+    size_t length = std::distance(begin, end);
+
+    if (length <= 1) {
+        return;
+    }
+
+    Iter middle = begin;
+    std::advance(middle, length >> 1);
+
+    mergeSort(begin, middle);
+    mergeSort(middle, end);
+
+    std::vector<typename Iter::value_type> buff;
+    buff.reserve(length);
+    Iter left = begin, right = middle;
+    while (left < middle || right < end) {
+        if (right >= end || left < middle && comp(*left, *right) <= 0) {
+            buff.push_back(std::move(*left));
+            ++left;
+        } else {
+            buff.push_back(std::move(*right));
+            ++right;
+        }
+    }
+
+    std::copy(std::make_move_iterator(buff.begin()), std::make_move_iterator(buff.end()), begin);
+}
+
---
+++ sorting/selection/selection.h
@@ -0,0 +1,18 @@
+#include <utility>
+#include "helpers.h"
+
+template <class Iter, class Comp = DefaultComparator<typename Iter::value_type>>
+void selectionSort(Iter begin, Iter end, const Comp& comp = Comp()) {
+    Iter start = begin;
+    while (start != end) {
+            Iter min = start;
+            for (Iter iter = start; iter != end; ++iter) {
+                if (comp(*min, *iter) > 0) {
+                    min = iter;
+                }
+            }
+            std::swap(*min, *start);
+            ++start;
+    }
+}
+
